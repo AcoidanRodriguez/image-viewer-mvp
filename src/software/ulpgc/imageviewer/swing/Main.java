9280b29ddc6e0bbe70f4dded4f6d81274d2f38e2
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-28 10:28:57 +0000

    Usamos FileImageLoader en el Main para la carga de imagenes

--- src/software/ulpgc/imageviewer/swing/Main.java
+++ src/software/ulpgc/imageviewer/swing/Main.java
@@ -1,14 +1,19 @@
 package software.ulpgc.imageviewer.swing;
 
+import software.ulpgc.imageviewer.FileImageLoader;
 import software.ulpgc.imageviewer.Image;
 import software.ulpgc.imageviewer.ImagePresenter;
 import software.ulpgc.imageviewer.mocks.MockImageLoader;
 
+import java.io.File;
+
 public class Main {
+    private static final String root = "";
+
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         ImagePresenter presenter = new ImagePresenter(frame.getImageDisplay());
-        presenter.show(image());
+        presenter.show(new FileImageLoader(new File(root)).load());
         frame.setVisible(true);
     }
 
