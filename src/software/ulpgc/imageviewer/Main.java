246bfca7e8b7d998ce8e2f8a632862a560f9412f
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-29 12:54:39 +0000

    Se usa el FileImageLoader en lugar de uno Mock y la ruta de las imagenes se obtienen del Main Frame

--- src/software/ulpgc/imageviewer/Main.java
+++ src/software/ulpgc/imageviewer/Main.java
@@ -5,12 +5,10 @@ import software.ulpgc.imageviewer.swing.MainFrame;
 import java.io.File;
 
 public class Main {
-    private static final String root = "C:/Users/acer/Pictures/Capturas";
-
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         ImagePresenter presenter = new ImagePresenter(frame.getImageDisplay());
-        presenter.show(new FileImageLoader(new File(root)).load());
+        presenter.show(new FileImageLoader(new File(frame.getPath())).load());
         frame.setVisible(true);
     }
 
