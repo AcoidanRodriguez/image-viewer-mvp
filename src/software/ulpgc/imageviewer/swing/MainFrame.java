60b80d07ef1cea19ec833283cfbe3d4b04cde4f7
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-29 13:23:42 +0000

    Pide la ruta donde obtener las imagenes

--- src/software/ulpgc/imageviewer/swing/MainFrame.java
+++ src/software/ulpgc/imageviewer/swing/MainFrame.java
@@ -2,8 +2,11 @@ package software.ulpgc.imageviewer.swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 
 public class MainFrame extends JFrame {
+    private final static String DefaultPath = "C:/Users/" + System.getProperty("user.name") + "/Pictures/";
     private ImageDisplay imageDisplay;
 
     public MainFrame()  {
@@ -18,9 +21,23 @@ public class MainFrame extends JFrame {
         return imageDisplay;
     }
 
+    public String getPath(){
+        String path = JOptionPane.showInputDialog("Ingrese la ruta donde están las imágenes: ");
+        return !path.equals("") && Files.exists(FileSystems.getDefault().getPath(path)) ?
+                path :
+                getDefaultPath(path);
+    }
+
+    private  String getDefaultPath(String path) {
+        JOptionPane.showMessageDialog(null,"La ruta " + path +
+                " no existe, se usará " + DefaultPath);
+        return DefaultPath;
+    }
+
     private Component createImageDisplay() {
         SwingImageDisplay display = new SwingImageDisplay();
         this.imageDisplay = display;
         return display;
     }
+
 }
